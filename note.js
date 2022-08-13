433a15d8e8bf9687d0ae5a3f7da18f9b6db40232
Pham Tai <bietdanhbomanh@gmail.com>
2022-08-13 21:47:39 +0700

    note

--- note.js
+++ note.js
@@ -1,23 +1,19 @@
-options = new chrome.Options();
-options.addArguments('headless'); // note: without dashes
-options.addArguments('disable-gpu');
+import { Builder, Browser, By, Key, until } from 'selenium-webdriver';
+import chrome from 'selenium-webdriver/chrome';
+import cheerio from 'cheerio';
+import config from './config';
+
+const options = new chrome.Options();
+// options.addArguments('headless');
 
 (async function example() {
     let driver = await new Builder().forBrowser(Browser.CHROME).setChromeOptions(options).build();
     try {
-        await driver.get('https://www.flashscore.com');
-        await driver.findElement(By.css('.filters__group .filters__tab:nth-child(2)')).click();
-
-        const page = await driver.findElement(By.css('html')).getAttribute('innerHTML');
-        const $ = cheerio.load(page);
-        const matchs = $('.event__match--live');
-        matchs.each(function () {
-            const id = $(this).attr('id');
-            const home = $(this).find('.event__participant--home').html();
-            const away = $(this).find('.event__participant--away').html();
-        });
-
-        await driver.wait(until.titleIs('wait'));
+        await driver.get('http://www.google.com/ncr');
+        // ép driver element thành element dom -> arguments[0]
+        await driver.executeScript('console.log(arguments[0])', driver.findElement(By.name('q')));
+        await driver.wait(until.titleIs('webdriver - Google Search'));
+        after(() => driver.quit());
     } finally {
         await driver.quit();
     }
